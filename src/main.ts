e5a84ba0c69cbdc5c319899814de2941ee954907
Muhamad Gameel <mgameel@pm.me>
2023-03-27 17:13:38 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import viteLogo from "/vite.svg";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>USSG</h1>
+  </div>
+`;
